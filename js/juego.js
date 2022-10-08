4c638e42077653bb032aa65ecf01a43f1fd6653f
Manel <quemonmanel@gmail.com>
2022-10-08 19:18:27 +0200

    Project03-game.js- functions and X/O position panel

---
+++ js/juego.js
@@ -0,0 +1,33 @@
+let tablero = [["O","O","X"],["","X","X"],["O","","X"]];
+
+const leerTablero = () => {
+    for(let x = 0; x < tablero.length; x++){
+
+        for(let i = 0; i < tablero.length; i++){
+     
+    
+            if(tablero[x][i] == "X"){
+             
+                console.log(`estoy en X = `, x, " y en Y = ", i, " aqui hay una X");
+            }else if (tablero[x][i] == "O") { 
+                console.log (`estoy en X = `, x, " y en Y = ", i, " aqui hay una O")
+            
+            }else{
+    
+                console.log (`estoy en X = `, x, " y en Y = ", i, " aqui no hay nada")
+            }
+    
+        }
+    
+    }
+
+};
+
+const checkVictoria = () => {
+
+  
+
+};
+
+
+leerTablero();
--- js/tablero.js
+++
@@ -1,4 +0,0 @@
-let tablero = [["O","O","X"],["","X","X"],["O","","X"]];
-
-const leerTablero = () => {
-    for(let x = 0; x < tablero.length; x++){
