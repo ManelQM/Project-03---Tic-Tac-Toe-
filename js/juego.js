08ad0f59527d146a43c777bf11588b38e68531ad
Manel <quemonmanel@gmail.com>
2022-10-09 00:41:52 +0200

    Project03-cambios en juego.js

--- js/juego.js
+++ js/juego.js
@@ -1,32 +1,34 @@
-let tablero = [["X","X","X"],["X","X","X"],["O","","X"]];
+let tablero = [["X","X",""],["","",""],["","X",""]];
+
 let turno = 0; 
 
 
 //Declaración tablero
-const leerTablero = () => {
-    for(let x = 0; x < tablero.length; x++){
+// const leerTablero = () => {
+//     for(let x = 0; x < tablero.length; x++){
 
-        for(let i = 0; i < tablero.length; i++){
-     
-    
-            if(tablero[x][i] == "X"){
-             
-                console.log(`estoy en X = `, x, " y en Y = ", i, " aqui hay una X");
-            }else if (tablero[x][i] == "O") { 
-                console.log (`estoy en X = `, x, " y en Y = ", i, " aqui hay una O")
+//         for(let i = 0; i < tablero.length; i++){
             
-            }else{
+//             if(tablero[x][i] == "X"){
+             
+//                 console.log(`estoy en X = `, x, " y en Y = ", i, " aqui hay una X");
+//             }else if (tablero[x][i] == "O") { 
+//                 console.log (`estoy en X = `, x, " y en Y = ", i, " aqui hay una O")
+            
+//             }else{
     
-                console.log (`estoy en X = `, x, " y en Y = ", i, " aqui no hay nada")
-            }
+//                 console.log (`estoy en X = `, x, " y en Y = ", i, " aqui no hay nada")
+//             }
     
-        }
+//         }
     
-    }
+//     }
 
-};
+// };
 
-leerTablero();
+// leerTablero();
+
+//función ganador
 
 const checkwinner = () => {
     //horizontales
@@ -41,7 +43,7 @@ const checkwinner = () => {
         alert ('has ganado')
     }else if (tablero [1][0] == tablero[1][1] && tablero[1][2]) {
         alert ('has ganado')
-    }else if (tablero[2][0] == talbero[2][1] && tablero[2][2]) {
+    }else if (tablero[2][0] == tablero[2][1] && tablero[2][2]) {
         alert ('has ganado')
     //diagonales    
     }else if ( tablero[0][0] == tablero[1][1] &tablero[2][2]){
@@ -49,10 +51,6 @@ const checkwinner = () => {
     }else if (tablero [2][0] == tablero[1][1] & tablero[0][2]){
         alert ('has ganado')
     }
-
-
-  
-
 };
 
 const turntime = () => {
@@ -60,6 +58,6 @@ const turntime = () => {
 
 }
 
-leerTablero();
+// leerTablero();
 
 checkwinner();
