39fd260b7695a712d7966fd0093ffdcdb21cf4b6
Manel <quemonmanel@gmail.com>
2022-10-09 00:11:40 +0200

    Project03- checkwinner function

--- js/juego.js
+++ js/juego.js
@@ -1,4 +1,5 @@
-let tablero = [["O","O","X"],["X","X","X"],["O","","X"]];
+let tablero = [["X","X","X"],["X","X","X"],["O","","X"]];
+let turno = 0; 
 
 const leerTablero = () => {
     for(let x = 0; x < tablero.length; x++){
@@ -23,8 +24,30 @@ const leerTablero = () => {
 
 };
 
+leerTablero();
 
-const checkVictoria = () => {
+const checkwinner = () => {
+    //horizontales
+    if (tablero[0][0] == tablero[1][0] && tablero [2][0]) {
+        alert ('has ganado'); 
+    }else if (tablero [0][1] == tablero[1][1] && tablero [2][1]){
+        alert ('has ganado');
+    }else if (tablero [0][2] == tablero [1][2] && tablero [2][2]) {
+        alert ('has ganado');
+    // verticales
+    }else if (tablero [0][0] == tablero [0][1] && tablero [0][2]) {
+        alert ('has ganado')
+    }else if (tablero [1][0] == tablero[1][1] && tablero[1][2]) {
+        alert ('has ganado')
+    }else if (tablero[2][0] == talbero[2][1] && tablero[2][2]) {
+        alert ('has ganado')
+    //diagonales    
+    }else if ( tablero[0][0] == tablero[1][1] &tablero[2][2]){
+        alert ('has ganado')
+    }else if (tablero [2][0] == tablero[1][1] & tablero[0][2]){
+        alert ('has ganado')
+    }
+
 
   
 
@@ -37,5 +60,4 @@ const turntime = () => {
 
 leerTablero();
 
-
-
+checkwinner();
---
+++ js/player.js
@@ -0,0 +1,22 @@
+class Player {
+
+    constructor(nombre,tipo){
+        this.numerofichas = 3;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    poner(){ 
+
+    }
+
+    desplazar(){
+
+    }
+
+};
+
+let player1 = new Player("Schopenhauer","human");
+let player2 = new Player("Trantor","cpu");
+
+console.log (player1);
