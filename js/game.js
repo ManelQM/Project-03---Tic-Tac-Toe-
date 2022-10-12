d2f4619aa4cca5b80e34a9a4f8a530e12410daf9
Manel <quemonmanel@gmail.com>
2022-10-13 00:13:14 +0200

    Project03-winner function not working so good. Last save before

--- js/game.js
+++ js/game.js
@@ -1,14 +1,94 @@
+let tablero = [["","",""],["","",""],["","",""]];
+let gamearena = Array.from(document.getElementsByClassName("activebox")); 
+let gameswitchbetweenplayers = true; 
+let interruptor = true;
+const gamecombowinner = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6], 
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]];
 
-let gamearena = Array.from(document.getElementsByClassName("activebox"));
-let gameswitchbetweenplayers = true;
+gamearena.map((gametile)=> {                      
+gametile.addEventListener("click",()=>{      
+if(gametile.innerHTML == ""){           
+gametile.innerHTML = (interruptor) ? "X" : "O"; 
+interruptor = !interruptor; 
+    };     
+  });
 
+ });
 
+ handleResultValidation(); 
+//  playerWins();
+//  const highLight = (combo) => {
+//     combo.forEach((idx) => gamearena[idx].classList.add("highlight"))
+//  }
+//  const playerWins = () =>{
+//     let checkWinner = gamecombowinner.find((combo)=>combo.every((idx) => gamearena[idx].textContent === gameswitchbetweenplayers));
+//     if(checkWinner){                    
+//         highLight(checkWinner);
+//         gameOver();
+//     }else if(boardFull()){
+//         alert("Tie!");
+//     }
+// };
 
+// const leerTablero = () => {
+//     for(let x = 0; x < gamearena.length; x++){
 
+//         for(let i = 0; i < gamearena.length; i++){ 
+//             checkwinner();
+//         }
+    
+//     }
+  
+// };
 
+// const checkwinner = () => {
+//     //horizontales
+//     if (tablero[0][0] == tablero[0][1] && tablero [0][2]) {
+//         alert ('has ganado'); 
+//     }else if (tablero [0][1] == tablero[1][1] && tablero [2][1]){
+//         alert ('has ganado');
+//     }else if (tablero [0][2] == tablero [1][2] && tablero [2][2]) {
+//         alert ('has ganado');
+//     // verticales
+//     }else if (tablero [0][0] == tablero [0][1] && tablero [0][2]) {
+//         alert ('has ganado')
+//     }else if (tablero [1][0] == tablero[1][1] && tablero[1][2]) {
+//         alert ('has ganado')
+//     }else if (tablero[2][0] == tablero[2][1] && tablero[2][2]) {
+//         alert ('has ganado')
+//     //diagonales    
+//     }else if ( tablero[0][0] == tablero[1][1] &&tablero[2][2]){
+//         alert ('has ganado')
+//     }else if (tablero [2][0] == tablero[1][1] && tablero[0][2]){
+//         alert ('has ganado')
+//     }
+// };  
+// leerTablero();
 
-
-
+function handleResultValidation() {
+    for (let i = 0; i < gamecombowinner.length; i++) { // Itera cada uno de las posibles combinaciones ganadores
+      const winCondition = gamecombowinner[i] // Guarda la combinación por ejemplo: [0, 1, 2]
+      let position1 = tablero[winCondition[0]],
+           position2 = tablero[winCondition[1]],
+           position3 = tablero[winCondition[2]] // Almacena el valor del estado actual del juego según las posiciones de winCondition
+  
+      if (position1 === '' || position2 === '' || position3 === '') {
+        continue; // Si hay algún valor vacio nadie ha ganado aún
+      }
+      if (position1 === position2 && position2 === position3) {
+        // Si todas las posiciones coinciden entonces, dicho jugador ha ganado la partida
+        alert("has ganado");
+        break
+      }
+    }
+}
 
 
 
