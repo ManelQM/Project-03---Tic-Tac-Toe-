5f9c7492b7272e6e33b18a6b678d82b949460127
Manel <quemonmanel@gmail.com>
2022-10-12 19:27:07 +0200

    Project03-winning function

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 
-let casillas = Array.from(document.getElementsByClassName("activebox"));
-// let player1 = new Player;
-// let player2 = new Player;
+let boxes = Array.from(document.getElementsByClassName("activebox"));
+let player1 = "";
+let player2 = "";
 let interruptor = true;
 
 
@@ -19,30 +19,29 @@ class Player {
 
 };
 
-
-
-
-
-casillas.map((casilla)=> {                      // Mapeo casilla - al ser un array recorre cada uno de los elementos
-    casilla.addEventListener("click",()=>{      // Función flecha que al efectuarse un click se ejecuta un condicional. 
-        if(casilla.innerHTML == ""){            // En dicho condicional se pondera que si el elemento casilla html está vacío 
-            casilla.innerHTML = (interruptor) ? "X" : "O"; // se puede poner ficha. 
+boxes.map((box)=> {                      // Mapeo casilla - al ser un array recorre cada uno de los elementos
+    box.addEventListener("click",()=>{      // Función flecha que al efectuarse un click se ejecuta un condicional. 
+        if(box.innerHTML == ""){            // En dicho condicional se pondera que si el elemento casilla html está vacío 
+            box.innerHTML = (interruptor) ? "X" : "O"; // se puede poner ficha. 
             interruptor = !interruptor; // Condición para que se produzca el switch entre jugadores 
             index++;
         };
     });
 });
 
+const leerTablero = () => {
+    for(let x = 0; x < tablero.length; x++){
 
+        for(let i = 0; i < tablero.length; i++){} // leer tablero llamará a checkwinner
+
+    }
+}    
 const checkwinner = () => {
     let casilla = casillas;
     casilla.forEach( cas => console.log (cas)); 
 }
 
-
-
-
-lines.onclick = () => {
+tiles.onclick = () => {
     document.getElementById 
 
 }
